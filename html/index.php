00198b97f0f7dca2f68a7ed698fdb1da34da5c63
austar94 <48815765+austar94@users.noreply.github.com>
2019-03-22 21:23:47 +0900

    Update index.php

--- html/index.php
+++ html/index.php
@@ -1,7 +1,11 @@
-<div class="img_upload">
-    <input type="file" name="imgFile_1" onchange="imgUploadSet(event, this)">
-    <img src="/img/no-images.png" class="img_1">
-</div>
+<form id="frm">
+    <div class="img_upload">
+        <input type="file" name="imgFile_1" onchange="imgUploadSet(event, this)">
+        <img src="/img/no-images.png" class="img_1">
+    </div>
+</form>
+<button type="button" onclick="uploadImgProc()">upload img</button>
 
 <script src="/js/img.js"></script>
 <script src="/js/exif-js.js"></script>
+
